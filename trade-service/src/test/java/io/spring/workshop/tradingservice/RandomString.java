f026a8a4e32bce7606bcc165621e493fb77ef895
musaid53 <muhammedcamkerten@gmail.com>
2018-12-17 00:30:21 +0300

    added test for trading-service: getQuotesFeed method

---
+++ trade-service/src/test/java/io/spring/workshop/tradingservice/RandomString.java
@@ -0,0 +1,22 @@
+package io.spring.workshop.tradingservice;
+
+import java.security.SecureRandom;
+
+public class RandomString {
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    /**
+     * Generates random string of given length from Base65 alphabet (numbers, lowercase letters, uppercase letters).
+     *
+     * @param count length
+     * @return random string of given length
+     */
+    public static String generate(int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; ++i) {
+            sb.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return sb.toString();
+    }
+}
--- trade-service/src/test/java/io/spring/workshop/tradingservice/TradingServiceApplicationTests.java
+++ trade-service/src/test/java/io/spring/workshop/tradingservice/TradingServiceApplicationTests.java
@@ -2,15 +2,76 @@ package io.spring.workshop.tradingservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TradingServiceApplicationTests {
 
+
+	@Autowired
+	private WebTestClient webTestClient;
+	@MockBean
+	private  QuotesClient mockClient;
 	@Test
 	public void contextLoads() {
+		System.out.println("Context loads");
+
+	}
+
+	@Test
+	public void getQuotesFeed(){
+
+		//QuotesClient mockClient = mock(QuotesClient.class);
+
+		//Create 25 quotes
+		when(mockClient.quotesFeed()).thenReturn(getRandomQutesFlux(25));
+		//when(mockClient.getLatestQuote("asdasd")).thenReturn(getMono());
+
+
+		//----/quotes/feed
+		List<Quote> results = webTestClient.get()
+				.uri("/quotes/feed")
+				.accept(MediaType.APPLICATION_STREAM_JSON)
+				.exchange()
+				.expectStatus().isOk()
+//				.expectHeader().contentType(MediaType.APPLICATION_STREAM_JSON_VALUE + ";charset=UTF-8")
+				.expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_STREAM_JSON)
+				.returnResult(Quote.class)
+				.getResponseBody()
+				.take(20)
+				.log(".Quote")
+				.collectList()
+				.block();
+
+		assertThat(results)
+				.allSatisfy(quote -> assertThat(quote.getPrice()).isNotNull());
+	}
+
+	private Flux<Quote> getRandomQutesFlux(final int count) {
+
+		List<Quote> quoteList = new ArrayList<>(20);
+		for (int i = 0; i <= count; i++) {
+			Quote quote =new Quote(RandomString.generate(4), new Random().nextDouble());
+			quoteList.add(quote);
+			System.out.println(quote.toString());
+		}
+		return Flux.fromIterable(quoteList);
 	}
 
 }
